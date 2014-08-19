efdfa445f52c445a554587fbae6a0da78ee1800c
Vivek R <vividvilla@gmail.com>
2014-08-19 23:11:42 +0530

    added transitions

--- js/scatterplot.js
+++ js/scatterplot.js
@@ -4,11 +4,11 @@ title.attr({"style": "color: #326DA8; font-style: italic; font-family: sans-seri
 
 var width = window.innerWidth;
 var height = window.innerHeight;
-var margin = "100";
+var margin = "50";
 
 var svg = body.append("svg").attr({
-    width: width - margin,
-    height: height - margin
+    width: width,
+    height: height
 });
 
 var data = [
@@ -31,15 +31,24 @@ var data = [
     {x: 19,  y: 52, radius : "25"},
 ]
 
+var minX = d3.min(data, function(d) { return d.x });
+var maxX = d3.max(data, function(d) { return d.x });
+var minY = d3.min(data, function(d) { return d.y });
+var maxY = d3.max(data, function(d) { return d.y });
+var minR = d3.min(data, function(d) { return d.radius });
+var maxR = d3.max(data, function(d) { return d.radius });
+
+console.log(minR,maxR)
+
 var xScale = d3.scale.linear()
     .domain([0, d3.max(data, function(d) { return d.x }) + 10])
-    .range([margin, width - 2*margin])
+    .range([margin, width - margin])
 
 var yScale = d3.scale.linear()
     .domain([0, d3.max(data, function(d) { return d.y }) + 10])
-    .range([margin, height - 2*margin])
+    .range([margin, height - margin])
 
-var yRadius = d3.scale.linear()
+var rScale = d3.scale.linear()
     .domain([0, d3.max(data, function(d) { return d.radius })])
     .range([0, 20])
 
@@ -58,13 +67,70 @@ svg.append("g").attr({
     transform: "translate("+[margin, 0]+")"
 }).call(yAxis);
 
-svg.selectAll("circle").
+var circleInitAttr = {
+        cx: function(d) { return xScale(d.x) },
+        cy: function(d) { return yScale(d.y) },
+        r: function(d) { return 0 },
+        fill: function(d, i) { return randomColor(i) }
+};
+
+var circleAttr = {
+        cx: function(d) { return xScale(d.x) },
+        cy: function(d) { return yScale(d.y) },
+        r: function(d) { return rScale(d.radius) },
+        fill: function(d, i) { return randomColor(i) }
+};
+
+var circle = svg.selectAll("circle").
     data(data).
     enter().
     append("circle").
     attr({
         cx: function(d) { return xScale(d.x) },
         cy: function(d) { return yScale(d.y) },
-        r: function(d) { return yRadius(d.radius) },
+        r: function(d) { return 0 },
         fill: function(d, i) { return randomColor(i) }
-    });
+    }).on("mouseover", mouseOverHandler)
+
+circle.transition()
+    .duration(function(d, i) { return i*300 })
+    .attr({
+        r: function(d) { return rScale(d.radius) },
+    })
+
+svg.on("click", function() {
+    var coordinates = d3.mouse(this);
+
+    var newData = {
+        x: Math.round(xScale.invert(coordinates[0])),
+        y: Math.round(xScale.invert(coordinates[1])),
+        radius: Math.round(rScale(Math.floor(Math.random() * (2*maxR-minR) + 2*minR)))
+    };
+
+    console.log("heeee", coordinates, newData);
+    data.push(newData);
+
+    var c = svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr(circleInitAttr)
+        .on("mouseover", mouseOverHandler);
+
+    c.transition()
+        .duration(1000)
+        .attr(circleAttr);
+});
+
+function mouseOverHandler(d,i) {
+        c = d3.select(this)
+        c.transition()
+        .duration(Math.random()*2000)
+        .ease("elastic")
+        .attr({
+            cx: function() { return xScale(Math.floor(Math.random() * (maxX-minX) + minX)); },
+            cy: function() { return yScale(Math.floor(Math.random() * (maxY-minY) + minY)); },
+            r: function() { return rScale(Math.floor(Math.random() * (maxR-30) + 30)*2); },
+            fill: function() { return randomColor(i) }
+        });
+    };
